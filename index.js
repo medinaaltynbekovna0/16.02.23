29143c498438cf09b7d3a27af51b180f3b479974
medina <damirovamedina38@gmail.com>
2023-02-19 22:09:20 +0300

    soloved new tasks

--- index.js
+++ index.js
@@ -104,15 +104,32 @@ console.log(carPrice[middle]);
 
 //1. Напишите функцию, которая выводит ваше имя и фамилию.
 // functionName("Erjan", "Kydyrov") // My  name is Erjan Kydyrov
+function myName(){
+	console.log("Medina")
+}
+myName();
+
 
 //2. Напишите функцию, которая выводит объем конуса по заданным параметрам - высота (height) и радиус (radius).
-// functionName(height, radius) // Объем конуса равен ...
+// functionName(hfunction myName(){
+
 
 //3. Напишите функцию, которая принимает строку и возвращает ее длину.
 // functionName(" ... ") // Длина строки равна ...
 
 //4. Напишите функцию, которая принимает число и проверяет, четное оно или нечетное.
 // functionName(num) // Число - четное || Число - нечетное
+function number (c) {
+	if (number % 2 === 0) {
+		console.log("this number is even");
+	}
+	else if (number % 2 === 1) {
+		console.log("this is not even");
+	}
+	else {
+		console.log("this is wrong command");
+	}
+}
 
 //5. Напишите функцию, которая возвращает квадрат числа.
 // functionName(num) // Квадрат этого числа равен
